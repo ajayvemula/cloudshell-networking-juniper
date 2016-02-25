128bb543da90d18f8f965c7e5f108872aaed4f4d
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2016-02-25 14:49:50 +0200

    Created JuniperBase

---
+++ cloudshell/networking/juniper/command_templates/commit_rollback.py
@@ -0,0 +1,4 @@
+from cloudshell.networking.parameters_service.command_template import CommandTemplate
+
+COMMIT_ROLLBACK = {'commit': CommandTemplate('commit', [], []),
+                   'rollback': CommandTemplate('rollback', [], [])}
