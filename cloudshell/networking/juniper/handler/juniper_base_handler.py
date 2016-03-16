3b553b73e5602ac4255bfa69d08c33a3695f39b1
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2016-03-16 15:16:44 +0200

    Fixed, adding error list

--- cloudshell/networking/juniper/handler/juniper_base_handler.py
+++ cloudshell/networking/juniper/handler/juniper_base_handler.py
@@ -143,11 +143,12 @@ class JuniperBaseHandler(HandlerBase):
         return output
 
     def _check_output_for_errors(self, output):
-        for error_pattern in self.ERROR_LIST:
+        for error_pattern in self._error_list:
             if re.search(error_pattern, output):
                 self.rollback()
                 raise Exception(
                     'Output contains error with pattern: "{0}", for output: "{1}"'.format(error_pattern, output))
+        return output
 
     def get_commands_list(self, command_map):
         prepared_commands = []
