ec6bd6d0855c3e8726992e8488f91e7369a62895
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2017-01-06 14:15:37 +0200

    Added shutdown

--- cloudshell/networking/juniper/runners/juniper_state_runner.py
+++ cloudshell/networking/juniper/runners/juniper_state_runner.py
@@ -1,5 +1,6 @@
 from cloudshell.networking.devices.runners.state_runner import StateRunner
 from cloudshell.networking.juniper.cli.juniper_cli_handler import JuniperCliHandler
+from cloudshell.networking.juniper.flows.juniper_shutdown_flow import JuniperShutdownFlow
 
 
 class JuniperStateRunner(StateRunner):
@@ -13,3 +14,9 @@ class JuniperStateRunner(StateRunner):
 
         super(JuniperStateRunner, self).__init__(logger, api, context)
         self._cli_handler = JuniperCliHandler(cli, context, logger, api)
+
+    @property
+    def shutdown_flow(self):
+        return JuniperShutdownFlow(self._cli_handler, self._logger)
+
+
