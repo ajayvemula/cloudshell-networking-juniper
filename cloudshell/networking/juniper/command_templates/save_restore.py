e2f833ab9fc45715503995ff2ef62d454e29639f
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2017-01-24 13:05:47 +0200

    Added error pattern

--- cloudshell/networking/juniper/command_templates/save_restore.py
+++ cloudshell/networking/juniper/command_templates/save_restore.py
@@ -1,7 +1,7 @@
 from collections import OrderedDict
 from cloudshell.cli.command_template.command_template import CommandTemplate
 
-ERROR_MAP = OrderedDict([(r'[Ee]rror:', 'Command error')])
+ERROR_MAP = OrderedDict([(r'[Ee]rror:', r'Command\s[Ee]rror', r'[Ee]rror\ssaving\sconfiguration')])
 
 SAVE = CommandTemplate('save {dst_path}', error_map=ERROR_MAP)
 RESTORE = CommandTemplate('load {restore_type} {src_path}', error_map=ERROR_MAP)
