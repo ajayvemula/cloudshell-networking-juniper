68ec464750ae233c53efd885779cfc5a5eba7acf
Yaroslav Nikonorov <yaroslav.n@qualisystems.com>
2017-01-26 15:10:26 +0200

    Fixed space in paths

--- cloudshell/networking/juniper/command_templates/save_restore.py
+++ cloudshell/networking/juniper/command_templates/save_restore.py
@@ -4,5 +4,5 @@ from cloudshell.cli.command_template.command_template import CommandTemplate
 ERROR_MAP = OrderedDict(
     [(r'[Ee]rror:', 'Command error'), (r'[Ee]rror\ssaving\sconfiguration', 'Error saving configuration')])
 
-SAVE = CommandTemplate('save {dst_path}', error_map=ERROR_MAP)
-RESTORE = CommandTemplate('load {restore_type} {src_path}', error_map=ERROR_MAP)
+SAVE = CommandTemplate('save "{dst_path}"', error_map=ERROR_MAP)
+RESTORE = CommandTemplate('load {restore_type} "{src_path}"', error_map=ERROR_MAP)
